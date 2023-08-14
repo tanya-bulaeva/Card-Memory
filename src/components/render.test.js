3aec8116654f471045e96c6ae5f5882d2b952e05
Tanya <t.akberdina@gmail.com>
2023-08-14 19:49:21 +0500

    unit test

--- src/components/render.test.js
+++ src/components/render.test.js
@@ -1,12 +1,12 @@
 const { it, expect, describe } = require("@jest/globals");
 //const { renderGame } = require("./renderGame");
 const { cardDeck }= require("./cardDeck")
-/*
-describe("function render game field", () => {
+
+/*describe("function render game field", () => {
 it ("should the field size from the selected level", () => {
 const level = 6;
 const render = renderGame(level)
-    expect(render).toHaveLength(6);
+    expect(render).toBe(6);
 });
 it ("returns NaN if level is not a number", () => {
     const level = "";
